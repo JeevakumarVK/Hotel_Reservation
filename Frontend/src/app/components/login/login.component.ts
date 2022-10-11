4d6fd612c8987086487ac339496c94fa3ac2253b
Jeeva Kumar <53359834+JeevakumarVK@users.noreply.github.com>
2022-10-11 10:16:07 +0530

    Add files via upload

---
+++ Frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+  loginForm = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email]),
+    pwd: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.maxLength(15),
+    ]),
+  }); 
+
+  loginSubmited() {
+    console.log(this.loginForm);
+  }
+
+  get Email(): FormControl{
+    return this.loginForm.get("email") as FormControl;
+  }
+  get PWD(): FormControl{
+    return this.loginForm.get("pwd") as FormControl;
+  }
+}
+
