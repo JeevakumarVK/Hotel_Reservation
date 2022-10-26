d38ff3ddeabb93eb4475b615656aed372de9e9ee
Jeeva Kumar <53359834+JeevakumarVK@users.noreply.github.com>
2022-10-26 19:22:42 +0530

    Add files via upload

---
+++ Hotelmanagement/Hotelmanagement/src/main/java/com/app/model/Client.java
@@ -0,0 +1,104 @@
+package com.app.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.*;
+
+@Entity
+public class Client {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public int userid;
+	private String  firstname;
+	private String  lastname;
+	private String   email;
+	private String  mobile;
+	private String  address;
+	private String  pwd;
+
+	public Client(int userid, String firstname, String lastname, String email, String mobile, String address, String pwd) {
+		this.userid = userid;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+		this.mobile = mobile;
+		this.address = address;
+		this.pwd = pwd;
+	}
+
+	public Client() {
+	}
+
+	public int getUserid() {
+		return userid;
+	}
+
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPwd() {
+		return pwd;
+	}
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+	@Override
+	public String toString() {
+		return "Client{" +
+				"userid=" + userid +
+				", firstname='" + firstname + '\'' +
+				", lastname='" + lastname + '\'' +
+				", email='" + email + '\'' +
+				", mobile='" + mobile + '\'' +
+				", address='" + address + '\'' +
+				", pwd='" + pwd + '\'' +
+				'}';
+	}
+}
